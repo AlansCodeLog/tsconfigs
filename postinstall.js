e508d0fbe2d9e551dd34e3b93d148079c828183c
Alan North <alanscodelog@gmail.com>
2024-10-08 18:51:17 -0300

    fix: fixed octal escape error

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
 let yellow = "\x1b[33m"
-let reset = "\033[0m"
+let reset = "\\033[0m"
 let message = yellow +
 `@alanscodelog/tsconfigs: Don't forget to run the install script:
 Note: This will overwrite "tsconfig.json" and "tsconfig.types.json"
